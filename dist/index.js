802683016b90ca39af0a1f93057d21b100984fc7
Sean <2136seanpaul@gmail.com>
2024-11-01 13:45:04 +0800

    First Commit

---
+++ dist/index.js
@@ -0,0 +1,6 @@
+"use strict";
+let sean = 20;
+if (sean < 50)
+    sean += 10;
+console.log(sean);
+//# sourceMappingURL=index.js.map
---
+++ src/index.ts
@@ -0,0 +1,5 @@
+let sean: number = 20;
+
+if (sean < 50) sean += 10;
+
+console.log(sean);
